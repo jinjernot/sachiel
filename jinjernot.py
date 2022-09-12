a95d075aab64fd87cc73e0037e62b9a318d4b2e9
norindes <norindes@gmail.com>
2022-09-11 22:20:21 -0500

    added GUI

--- jinjernot.py
+++ jinjernot.py
@@ -1,73 +1,141 @@
+from logging import root
 from time import sleep
 import time
+from turtle import color
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import urllib.request
 import os
 import webbrowser
+from tkinter import *
+import tkinter as tk
 
-print("Bienvenido a Jinjernot")
-user = input("Ingresa el usuario a estalkear  ")
 
-browser = webdriver.Firefox()
-browser.implicitly_wait(5)
 
-browser.get('https://www.instagram.com/')
+#GUI
+ws = Tk()
+ws.title('Jinjernot')
+ws.geometry('1920x1080')
 
-sleep(2)
+user = tk.StringVar()
 
-#Log in
+def stalker():
+    
+    browser = webdriver.Firefox()
+    browser.implicitly_wait(5)
 
-username_input = browser.find_element(By.CSS_SELECTOR, "input[name='username']")
-password_input = browser.find_element(By.CSS_SELECTOR, "input[name='password']")
+    browser.get('https://www.instagram.com/')
 
-username_input.send_keys("")
-password_input.send_keys("")
+    sleep(2)
 
-login_link = browser.find_element("xpath", "//div[text()='Log In']")
-login_link.click()
+    #   Log in
 
-time.sleep(3)
+    username_input = browser.find_element(By.CSS_SELECTOR, "input[name='username']")
+    password_input = browser.find_element(By.CSS_SELECTOR, "input[name='password']")
 
-#----
-passlogin_link = browser.find_element("xpath", "//button[text()='Not Now']")
-passlogin_link.click()
+    username_input.send_keys("")
+    password_input.send_keys("")
 
-time.sleep(3)
+    login_link = browser.find_element("xpath", "//div[text()='Log In']")
+    login_link.click()
 
-passlogin_link = browser.find_element("xpath", "//button[text()='Not Now']")
-passlogin_link.click()
+    time.sleep(3)
 
-time.sleep(3)
+    #----
+    passlogin_link = browser.find_element("xpath", "//button[text()='Not Now']")
+    passlogin_link.click()
 
-search_user = browser.find_element("xpath", "//input[@placeholder='Search']")
-search_user.send_keys(user)
-time.sleep(3)
-search_user.send_keys(Keys.ENTER)
-search_user.send_keys(Keys.ENTER)
+    time.sleep(3)
 
-time.sleep(3)
+    passlogin_link = browser.find_element("xpath", "//button[text()='Not Now']")
+    passlogin_link.click()
 
-folder_name = user
-folder = "im"
-os.mkdir(folder_name)
-os.mkdir(folder)
+    time.sleep(3)
 
-images = browser.find_elements(By.TAG_NAME,'img')
+    search_user = browser.find_element("xpath", "//input[@placeholder='Search']")
+    search_user.send_keys(user.get())
+    time.sleep(3)
+    search_user.send_keys(Keys.ENTER)
+    search_user.send_keys(Keys.ENTER)
 
-i=0
-for x in images:
-    ext = ".jpg"
-    src = x.get_attribute("src")
-    time.sleep(1)
-    path = (f'{folder_name}/{folder_name}{i}{ext}')
-    path2 = ((f'{folder}/{i}{ext}'))
-    chida = urllib.request.urlretrieve(src, path)
-    palreact = urllib.request.urlretrieve(src, path2)
-    print(chida)
-    print(palreact)
-    i=i+1
-browser.close()
+    time.sleep(3)
 
-webbrowser.open_new_tab('jinjernot.html')
+    folder_name = user.get()
+    folder = "im"
+    os.mkdir(folder_name)
+    os.mkdir(folder)
+
+    images = browser.find_elements(By.TAG_NAME,'img')
+
+    i=0
+    for x in images:
+        ext = ".jpg"
+        src = x.get_attribute("src")
+        time.sleep(1)
+        path = (f'{folder_name}/{folder_name}{i}{ext}')
+        path2 = ((f'{folder}/{i}{ext}'))
+        chida = urllib.request.urlretrieve(src, path)
+        palreact = urllib.request.urlretrieve(src, path2)
+        print(chida)
+        print(palreact)
+        i=i+1
+    browser.close()
+
+    webbrowser.open_new_tab('jinjernot.html')
+
+bg = PhotoImage(file = 'img/intro.png')
+
+canvas = Canvas(
+	ws, 
+	width = 1920,
+	height = 1080
+	)
+
+canvas.pack(fill='both', expand = True)
+
+canvas.create_image(
+	0, 
+	0, 
+	image=bg,
+	anchor = "nw"
+	)
+
+canvas.create_text(
+	1000, 
+	100, 
+	text = 'Ingresa el usuario a stalkear',
+	font=('Verdana', 36),
+	)
+
+entry = Entry (
+    ws,
+    width=50,
+    textvariable=user,
+)
+
+entry_canvas = canvas.create_window(
+    950,
+    200,
+    window=entry
+    )
+
+
+btn = Button(
+	ws, 
+	text = 'Stalkear',
+	command=stalker,
+	width=10,
+	height=5,
+	relief=SOLID,
+	font=('arial', 36)
+	)
+
+btn_canvas = canvas.create_window(
+	800, 
+	300,
+	anchor = "nw",
+	window = btn,
+	)
+
+ws.mainloop()
