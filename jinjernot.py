fd9ecf708c2a7a97667521e599cac4c339b803b0
norindes <norindes@gmail.com>
2022-12-18 03:25:18 -0600

    funcion main y clean code

--- jinjernot.py
+++ jinjernot.py
@@ -13,7 +13,7 @@ import tkinter as tk
 driver = webdriver.Firefox()
 
 #funcion para logearse a instagram
-def login():
+def instalogin():
     
     driver.implicitly_wait(5)
     driver.get('https://www.instagram.com/')
@@ -21,33 +21,22 @@ def login():
     sleep(3)
 
     #Busca los campos para meter user y contraseña
-    username_input = driver.find_element(By.CSS_SELECTOR, "input[name='username']")
-    password_input = driver.find_element(By.CSS_SELECTOR, "input[name='password']")
-
-    username_input.send_keys("")
-    password_input.send_keys("")
-
-    login_link = driver.find_element("xpath", "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button/div")
-    login_link.click()
-
+    login = driver.find_element(By.CSS_SELECTOR, "input[name='username']").send_keys("")
+    login = driver.find_element(By.CSS_SELECTOR, "input[name='password']").send_keys("")
+    login = driver.find_element("xpath", "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button/div").click()
     time.sleep(3)
 
     #acepta pop ups
-    passlogin_link = driver.find_element("xpath", "//button[text()='Not Now']")
-    passlogin_link.click()
-
+    login = driver.find_element("xpath", "//button[text()='Not Now']").click()
     time.sleep(3)
-
-    passlogin_link = driver.find_element("xpath", "//button[text()='Not Now']")
-    passlogin_link.click()
-
+    login = driver.find_element("xpath", "//button[text()='Not Now']").click()
     time.sleep(3)
 
 #funcion Stalkergram
 def stalker():
 
     #Llama a la funcion para logear
-    login()
+    instalogin()
 
     #Busca al usuario que se metio en el GUI
     search_user = driver.find_element("xpath", "//input[@placeholder='Search']")
@@ -55,7 +44,6 @@ def stalker():
     time.sleep(3)
     search_user.send_keys(Keys.ENTER)
     search_user.send_keys(Keys.ENTER)
-
     time.sleep(3)
 
     #Crea un folder para ese user
@@ -88,7 +76,7 @@ def stalker():
 #funcion like
 def like():
 
-    login()
+    instalogin()
 
     likes = driver.find_element("xpath", "//input[@placeholder='Search']")
     likes.send_keys(user.get())
@@ -102,28 +90,23 @@ def like():
 def inbox():
 
     #Llama a la funcion para logear
-
-    login()
+    instalogin()
 
     time.sleep(3)
-    mensaje = driver.find_element("xpath", "//a[@href='/direct/inbox/']/./..")
-    mensaje.click()
+    mensaje = driver.find_element("xpath", "//a[@href='/direct/inbox/']/./..").click()
 
     time.sleep(2)
-    mensaje = driver.find_element("xpath", "//button[text()='Send Message']")
-    mensaje.click()
+    mensaje = driver.find_element("xpath", "//button[text()='Send Message']").click()
 
     time.sleep(2)
     mensaje = driver.find_element("xpath", "//input[@placeholder='Search...']")
     mensaje.send_keys(user.get())
 
     time.sleep(3)
-    mensaje = driver.find_element("xpath", "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[2]/div[2]/div[1]/div/div[1]/span/img")
-    mensaje.click()
+    mensaje = driver.find_element("xpath", "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[2]/div[2]/div[1]/div/div[1]/span/img").click()
     
-    time.sleep(3)
-    mensaje = driver.find_element("xpath", "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[3]/div/button/div")
-    mensaje.click()
+    time.sleep(5)
+    mensaje = driver.find_element("xpath", "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[3]/div/button/div").click()
 
     time.sleep(3)
     mensaje = driver.find_element("xpath", "//textarea[@placeholder='Message...']")
@@ -133,7 +116,8 @@ def inbox():
     time.sleep(3)
     driver.close()
 
-def tweet():
+#
+def twitterlogin():
 
     driver.implicitly_wait(5)
     driver.get('https://www.twitter.com/')
@@ -149,133 +133,137 @@ def tweet():
     tweet.send_keys("")
     tweet.send_keys(Keys.ENTER)
     sleep(5)
+
+def tweet():
+
+    twitterlogin()
+
     tweet = driver.find_element("xpath", "//div[@data-testid='tweetTextarea_0']")
     tweet.send_keys("test")
     sleep(3)
     tweet = driver.find_element("xpath", "//div[@data-testid='tweetButtonInline']").click()
 
+def main():
+    #Crea el GUI
+    ws = Tk()
+    ws.title('Jinjernot')
+    ws.geometry('1920x1080')
+    global user
+    user = tk.StringVar()
 
 
+    # Carga imagen de fondo png
+    bg = PhotoImage(file = 'img/intro.png')
 
+    #crea Ventana HD
+    canvas = Canvas(
+        ws, 
+        width = 1920,
+        height = 1080
+        )
 
+    #Asigna la img a todo el fondo
+    canvas.pack(fill='both', expand = True)
 
-#Crea el GUI
-ws = Tk()
-ws.title('Jinjernot')
-ws.geometry('1920x1080')
-user = tk.StringVar()
+    canvas.create_image(
+        0, 
+        0, 
+        image=bg,
+        anchor = "nw"
+        )
 
+    #Titulo
+    canvas.create_text(
+        1000, 
+        110, 
+        text = 'Jinjernot v1',
+        font=("Terminal", 36, "bold"),
+        fill=("white")
+        )
 
-# Carga imagen de fondo png
-bg = PhotoImage(file = 'img/intro.png')
-
-#crea Ventana HD
-canvas = Canvas(
-	ws, 
-	width = 1920,
-	height = 1080
-	)
-
-#Asigna la img a todo el fondo
-canvas.pack(fill='both', expand = True)
-
-canvas.create_image(
-	0, 
-	0, 
-	image=bg,
-	anchor = "nw"
-	)
-
-#Titulo
-canvas.create_text(
-	1000, 
-	110, 
-	text = 'Jinjernot v1',
-	font=("Terminal", 36, "bold"),
-    fill=("white")
-	)
-
-#Agarra el usuario y lo asigna a variable
-entry = Entry (
-    ws,
-    width=50,
-    textvariable=user,
-)
-entry_canvas = canvas.create_window(
-    950,
-    200,
-    window=entry
+    #Agarra el usuario y lo asigna a variable
+    entry = Entry (
+        ws,
+        width=50,
+        textvariable=user,
     )
+    entry_canvas = canvas.create_window(
+        950,
+        200,
+        window=entry
+        )
 
-#boton stalker
-btnS = Button(
-	ws, 
-	text = 'Stalk',
-	command=stalker,
-	width=8,
-	height=2,
-	relief=SOLID,
-	font=("Terminal", 36, "bold")
-	)
-btns_canvas = canvas.create_window(
-	800, 
-	300,
-	anchor = "nw",
-	window = btnS,
-	)
+    #boton stalker
+    btnS = Button(
+        ws, 
+        text = 'Stalk',
+        command=stalker,
+        width=8,
+        height=2,
+        relief=SOLID,
+        font=("Terminal", 36, "bold")
+        )
+    btns_canvas = canvas.create_window(
+        800, 
+        300,
+        anchor = "nw",
+        window = btnS,
+        )
 
-#Boton Inbox
-btnI = Button(
-	ws, 
-	text = 'Inbox',
-	command=inbox,
-	width=8,
-	height=2,
-	relief=SOLID,
-	font=("Terminal", 36, "bold")
-	)
+    #Boton Inbox
+    btnI = Button(
+        ws, 
+        text = 'Inbox',
+        command=inbox,
+        width=8,
+        height=2,
+        relief=SOLID,
+        font=("Terminal", 36, "bold")
+        )
 
-btnI_canvas = canvas.create_window(
-	400, 
-	300,
-	anchor = "nw",
-	window = 
-    btnI,
-	)
+    btnI_canvas = canvas.create_window(
+        400, 
+        300,
+        anchor = "nw",
+        window = 
+        btnI,
+        )
 
-btnL = Button(
-	ws, 
-	text = 'Likes',
-	command=like,
-	width=8,
-	height=2,
-	relief=SOLID,
-	font=("Terminal", 36, "bold")
-	)
-btnL_canvas = canvas.create_window(
-	400, 
-	600,
-	anchor = "nw",
-	window = btnL,
-	)
-
-
-btnL = Button(
-	ws, 
-	text = 'Tweet',
-	command=tweet,
-	width=8,
-	height=2,
-	relief=SOLID,
-	font=("Terminal", 36, "bold")
-	)
-btnL_canvas = canvas.create_window(
-	1200, 
-	300,
-	anchor = "nw",
-	window = btnL,
-	)
+    btnL = Button(
+        ws, 
+        text = 'Likes',
+        command=like,
+        width=8,
+        height=2,
+        relief=SOLID,
+        font=("Terminal", 36, "bold")
+        )
+    btnL_canvas = canvas.create_window(
+        400, 
+        600,
+        anchor = "nw",
+        window = btnL,
+        )
 
 
-ws.mainloop()
+    btnL = Button(
+        ws, 
+        text = 'Tweet',
+        command=tweet,
+        width=8,
+        height=2,
+        relief=SOLID,
+        font=("Terminal", 36, "bold")
+        )
+    btnL_canvas = canvas.create_window(
+        1200, 
+        300,
+        anchor = "nw",
+        window = btnL,
+        )
 
+
+    ws.mainloop()
+
+if __name__ == "__main__":
+    main()
